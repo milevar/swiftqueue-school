47bf3d5f706ac23d7e6c43513838b1445920acfc
Milena Ramirez <milena.ramirez.lopez@gmail.com>
2024-07-18 20:59:44 +0100

    Database files

---
+++ db/data.sql
@@ -0,0 +1,9 @@
+insert into users (name, email, password)
+values
+    ('Melissa','melissa@email.com', '123456'),
+    ('Milena', 'mile@email.com', 'secret');
+
+insert into courses (name, status, start_date, end_date)
+values
+    ('PHP OOP', 'Active', '12/08/2024', '18/09/2024'),
+    ('Laravel', 'Inactive', '22/07/2024', '15/10/2024');
